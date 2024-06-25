4ea7864b97627e2d3a42fd8d38c8ca4d54a1c3dc
희성 <ghkdgmltjd33@naver.com>
2024-06-25 14:59:31 +0900

    ui수정(loginscreen)

--- app/src/main/java/com/example/team_project/loginsceen/LoginScreen.kt
+++ app/src/main/java/com/example/team_project/loginsceen/LoginScreen.kt
@@ -1,6 +1,8 @@
 package com.example.team_project.loginsceen
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,16 +10,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.team_project.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -32,13 +45,46 @@ fun LoginScreen(navController: NavController) {
     val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
 
+    // 연보라색 정의
+    val lightPurple = Color(0xFFE1BEE7)
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
+            .padding(16.dp)
+            .background(Color.White),
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Cracker",
+            fontFamily = FontFamily.SansSerif,
+            fontStyle = FontStyle.Normal,
+            fontWeight = FontWeight.Normal,
+            fontSize = 64.sp,
+            lineHeight = 77.sp,
+            color = Color.Black,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.house),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .width(264.dp)
+                .height(244.dp)
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
         TextField(
             value = email,
             onValueChange = {},
@@ -46,7 +92,9 @@ fun LoginScreen(navController: NavController) {
             modifier = Modifier.fillMaxWidth(),
             enabled = false // 값을 변경하지 못하도록 비활성화
         )
+
         Spacer(modifier = Modifier.height(8.dp))
+
         TextField(
             value = password,
             onValueChange = {},
@@ -54,21 +102,27 @@ fun LoginScreen(navController: NavController) {
             modifier = Modifier.fillMaxWidth(),
             enabled = false // 값을 변경하지 못하도록 비활성화
         )
+
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            coroutineScope.launch {
-                try {
-                    auth.signInWithEmailAndPassword(email, password).await()
-                    navController.navigate("mainScreen")
-                } catch (e: Exception) {
-                    Toast.makeText(
-                        context,
-                        "Authentication failed: ${e.message}",
-                        Toast.LENGTH_SHORT
-                    ).show()
+
+        Button(
+            onClick = {
+                coroutineScope.launch {
+                    try {
+                        auth.signInWithEmailAndPassword(email, password).await()
+                        navController.navigate("mainScreen")
+                    } catch (e: Exception) {
+                        Toast.makeText(
+                            context,
+                            "Authentication failed: ${e.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
-            }
-        }) {
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = lightPurple),
+            modifier = Modifier.fillMaxWidth() // 텍스트 필드와 버튼의 너비를 맞춤
+        ) {
             Text("Login")
         }
     }
--- app/src/main/java/com/example/team_project/resultscreen/ResultSceen.kt
+++ app/src/main/java/com/example/team_project/resultscreen/ResultSceen.kt
@@ -2,8 +2,13 @@ package com.example.team_project.resultscreen
 
 import android.net.Uri
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -17,30 +22,49 @@ import coil.compose.rememberAsyncImagePainter
 @Composable
 fun ResultScreen(navController: NavController, encodedImageUrl: String) {
     val imageUrl = Uri.decode(encodedImageUrl)
-    Box(
-        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
-    ) {
-        Image(
-            painter = rememberAsyncImagePainter(imageUrl),
-            contentDescription = "Detection Result",
-            modifier = Modifier.fillMaxSize()
-        )
 
-        Button(
-            onClick = { navController.navigate("cameraScreen") },
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = rememberAsyncImagePainter(imageUrl),
+                contentDescription = "Detection Result",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            )
+        }
+
+        Row(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
-                .padding(16.dp)
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text("Back to Camera")
-        }
-        Button(
-            onClick = { navController.navigate("infoScreen") },
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(16.dp)
-        ) {
-            Text("Info")
+            Button(
+                onClick = { navController.navigate("cameraScreen") },
+                modifier = Modifier
+                    .weight(1f)
+                    .height(48.dp)
+                    .padding(end = 8.dp)
+            ) {
+                Text("Back to Camera")
+            }
+            Button(
+                onClick = { navController.navigate("infoScreen") },
+                modifier = Modifier
+                    .weight(1f)
+                    .height(48.dp)
+                    .padding(start = 8.dp)
+            ) {
+                Text("Info")
+            }
         }
     }
 }
